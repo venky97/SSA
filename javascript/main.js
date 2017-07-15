cf070c89882a92dac31fcdb7162115e929a44b1a
venky97 <venkateshwaran12345@gmail.com>
2017-07-15 13:22:36 +0530

    UI changes

--- javascript/main.js
+++ javascript/main.js
@@ -28,33 +28,7 @@ $(document).ready(function() {
 }, function () {
     this.start();
 });
+$('#bird').click(function(){
+  $('#play').attr("src":"media/aud/bird.wav");
 });
-/*
-$('#marqi').marquee('events_marquee').mouseover(function () {
-  $(this).trigger('stop');
-}).mouseout(function () {
-  $(this).trigger('start');
-}).mousemove(function (event) {
-  if ($(this).data('drag') == true) {
-    this.scrollLeft = $(this).data('scrollX') + ($(this).data('x') - event.clientX);
-  }
-}).mousedown(function (event) {
-  $(this).data('drag', true).data('x', event.clientX).data('scrollX', this.scrollLeft);
-}).mouseup(function () {
-  $(this).data('drag', false);
 });
-      // basic version is: $('div.demo marquee').marquee() - but we're doing some sexy extras
-      $('div.demo marquee').marquee('pointer').mouseover(function () {
-          $(this).trigger('stop');
-      }).mouseout(function () {
-          $(this).trigger('start');
-      }).mousemove(function (event) {
-          if ($(this).data('drag') == true) {
-              this.scrollLeft = $(this).data('scrollX') + ($(this).data('x') - event.clientX);
-          }
-      }).mousedown(function (event) {
-          $(this).data('drag', true).data('x', event.clientX).data('scrollX', this.scrollLeft);
-      }).mouseup(function () {
-          $(this).data('drag', false);
-      });
-*/
